b111ab7580102764a6e39e8f5431969284b0f3a9
Practical Dev <femi.obatoki@gmail.com>
2024-08-03 00:56:01 +0100

    Cluster Infrastructure

---
+++ modules/eks/main.tf
@@ -0,0 +1,64 @@
+resource "aws_security_group" "my_sg_config" {
+  name        = var.sg_name
+  description = "Security group for additional access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
+
+
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+    security_group_ids = [aws_security_group.my_sg_config.id]
+  }
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = var.node_group_name
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  tags = {
+    Name = var.node_group_name
+  }
+}
