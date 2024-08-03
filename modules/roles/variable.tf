c62dec0061e1a6624b10ad098967f94ae6cde19c
Practical Dev <femi.obatoki@gmail.com>
2024-08-03 01:07:21 +0100

    Cluster Infrastructure

---
+++ modules/roles/variable.tf
@@ -0,0 +1,10 @@
+
+variable "cluster_role_name" {
+  type =  string
+  description = "The name of the EKS cluster role"
+}
+
+variable "node_role_name" {
+  type =  string
+  description = "The name of the node EKS role"
+}
