945257cc0405ccc1f0bea00116ee9e27d22b643c
Practical Dev <femi.obatoki@gmail.com>
2024-08-03 01:05:50 +0100

    Cluster Infrastructure

---
+++ modules/vpc/output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets"
+  value       = aws_subnet.public_subnet.*.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "route_table_id" {
+  description = "The ID of the route table"
+  value       = aws_route_table.public.id
+}
