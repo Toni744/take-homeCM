1727f1fe0754751667f20fbc4c885e95d83da615
Practical Dev <femi.obatoki@gmail.com>
2024-08-03 01:02:41 +0100

    Cluster Infrastructure

---
+++ modules/vpc/main.tf
@@ -0,0 +1,62 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = var.subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_cidrs, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+/*
+resource "aws_subnet" "private_subnet" {
+  count                   = var.subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.private_cidrs, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  
+
+  tags = {
+    Name = "private-subnet-db-${count.index}"
+  }
+}
+*/
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = var.subnet_count
+  subnet_id      = aws_subnet.public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+data "aws_availability_zones" "available" {}
